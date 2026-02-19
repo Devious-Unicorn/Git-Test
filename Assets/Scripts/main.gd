5cf4d0b8e17cfcba0a506373287c358d687b9b85
Glenn Lawrence <gslawrence27@gmail.com>
2026-02-18 19:47:04 -0700

    Add basic script to Main and change window size

--- Assets/Scripts/main.gd
+++ Assets/Scripts/main.gd
@@ -3,6 +3,10 @@ extends Control
 @onready var packedScenes: Dictionary = {
 	"main menu": preload("res://Assets/Scenes/main_menu.tscn")
 }
+@onready var scenes: Dictionary = {
+	"main menu": null
+}
 
 func _ready() -> void:
-	pass
+	scenes["main menu"] = packedScenes["main menu"].instantiate()
+	add_child(scenes["main menu"])
